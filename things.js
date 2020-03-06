026d89e9a5108d97230501bc23a7b98c6355d5b4
Naheel-Azawy <naheelazawy@gmail.com>
2020-03-06 17:52:53 +0300

    things

---
+++ things.js
@@ -0,0 +1,113 @@
+const things = {
+    "Simple-Computer-Simulator": {
+        "tags": "hardware,java",
+        "text": "Simulate a simple computer architecture",
+        "img": "https://github.com/Naheel-Azawy/Simple-Computer-Simulator/raw/master/screenshots/gui-dark.png"
+    },
+    "arduino-lcd-race-game": {
+        "tags": "hardware,c",
+        "text": "A little brick race game for Arduino",
+        "img": "https://github.com/Naheel-Azawy/arduino-lcd-race-game/raw/master/img.jpg"
+    },
+    "ducky2digi": {
+        "tags": "hardware,python",
+        "text": "Convert USB-Rubber-Ducky’s scripts to Arduino sketches for Digispark",
+        "img": "https://s3.amazonaws.com/digistump-resources/images/l/62779e34f1553179532b56e213a64f21.jpg"
+    },
+    "fish-complete-arduino-cli": {
+        "tags": "hardware,fish",
+        "text": "Fish completion for arduino-cli",
+        "img": "https://avatars2.githubusercontent.com/u/30007275"
+    },
+    "Executor": {
+        "tags": "python",
+        "text": "Run everything like a script!",
+        "img": "https://github.com/Naheel-Azawy/Executor/raw/master/screenshot-emacs.png"
+    },
+    "mandelbrot": {
+        "tags": "c",
+        "text": "Mandelbrot set and Julia set written purely in C - Mbed compatible",
+        "img": "https://raw.githubusercontent.com/Naheel-Azawy/naheel-azawy.github.io/master/bg/wall4.png"
+    },
+    "chats-mixer": {
+        "tags": "javascript",
+        "text": "Mixes chats from different services",
+        "img": "https://github.com/Naheel-Azawy/chats-mixer/raw/master/screenshots/ab.png"
+    },
+    "simplex-calc": {
+        "tags": "javascript",
+        "text": "",
+        "img": ""
+    },
+    "moon-script": {
+        "tags": "sh",
+        "text": "Tiny scripts to generate lunar phases GIF",
+        "img": ""
+    },
+    "darken-css": {
+        "tags": "python",
+        "text": "Darken CSS already dark style sheets",
+        "img": "https://github.com/Naheel-Azawy/darken-css/raw/master/screenshot.png"
+    },
+    "rdf-generate": {
+        "tags": "javascript",
+        "text": "Converts structured and semi-structured data to RDF",
+        "img": "https://github.com/Naheel-Azawy/rdf-generate/raw/master/screenshot.png"
+    },
+    "emacs-arabic-reshaper": {
+        "tags": "emacs-lisp",
+        "text": "Reshape Arabic characters in Emacs",
+        "img": ""
+    },
+    "c-arabic-reshaper": {
+        "tags": "c",
+        "text": "A simple Arabic reshaper written in C",
+        "img": ""
+    },
+    "BinaryGame": {
+        "tags": "vala",
+        "text": "A tiny binary game written in Vala",
+        "img": "https://github.com/Naheel-Azawy/BinaryGame/raw/master/screenshots/s2.png"
+    },
+    "naheel-dotfiles": {
+        "tags": "desktop,sh",
+        "text": "A collection of toys glued together to buildup my setup on my main machine",
+        "img": "https://github.com/Naheel-Azawy/naheel-dotfiles/raw/master/screenshots/1.png"
+    },
+    "theterm": {
+        "tags": "desktop,sh",
+        "text": "A wrapper script that gives terminals super powers",
+        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Xterm.png/330px-Xterm.png"
+    },
+    "stpv": {
+        "tags": "desktop,sh",
+        "text": "",
+        "img": ""
+    },
+    "cp-p": {
+        "tags": "desktop,sh",
+        "text": "",
+        "img": ""
+    },
+    "conky-config": {
+        "tags": "desktop,sh",
+        "text": "",
+        "img": ""
+    },
+    "c-tricks": {
+        "tags": "c",
+        "text": "",
+        "img": ""
+    },
+    "c-oop": {
+        "tags": "c",
+        "text": "",
+        "img": ""
+    },
+    "the-super-tiny-compiler-in-c": {
+        "tags": "c",
+        "text": "",
+        "img": ""
+    }
+};
+
