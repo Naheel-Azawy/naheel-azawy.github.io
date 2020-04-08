015f173237a4046036d845f7356e48a7f57f6df9
Naheel-Azawy <naheelazawy@gmail.com>
2020-04-08 20:51:46 +0300

    things

--- things.js
+++ things.js
@@ -5,36 +5,42 @@ const things = {
         "text": "A simple CLI Quran",
         "img": "https://raw.githubusercontent.com/Naheel-Azawy/quran/master/screenshot.png"
     },
+
     "Simple-Computer-Simulator": {
         "class": "A",
         "tags": "hardware,java",
-        "text": "Simulate a simple computer architecture",
+        "text": "A simple computer architecture simulator and assembler",
         "img": "https://github.com/Naheel-Azawy/Simple-Computer-Simulator/raw/master/screenshots/gui-dark.png"
     },
+
     "arduino-lcd-race-game": {
         "class": "A",
         "tags": "hardware,c",
         "text": "A little brick race game for Arduino",
         "img": "https://github.com/Naheel-Azawy/arduino-lcd-race-game/raw/master/img.jpg"
     },
+
     "ducky2digi": {
         "class": "A",
         "tags": "hardware,python",
         "text": "Convert USB-Rubber-Ducky’s scripts to Arduino sketches for Digispark",
         "img": "https://s3.amazonaws.com/digistump-resources/images/l/62779e34f1553179532b56e213a64f21.jpg"
     },
+
     "fish-complete-arduino-cli": {
         "class": "C",
         "tags": "hardware,fish",
         "text": "Fish completion for arduino-cli",
         "img": "https://avatars2.githubusercontent.com/u/30007275"
     },
+
     "runner": {
         "class": "A",
         "tags": "v",
         "text": "Run everything like a script!",
         "img": "https://raw.githubusercontent.com/Naheel-Azawy/runner/master/imgs/screenshot-emacs.png"
     },
+
     "boat-code": {
         "class": "A",
         "tags": "javascript",
@@ -42,18 +48,21 @@ const things = {
         "img": "https://raw.githubusercontent.com/Naheel-Azawy/boat-code/master/screenshot.png",
         "link": "https://naheel-azawy.github.io/boat-code/"
     },
+
     "mandelbrot": {
         "class": "B",
         "tags": "c",
         "text": "Mandelbrot set and Julia set written purely in C - Mbed compatible",
         "img": "https://raw.githubusercontent.com/Naheel-Azawy/naheel-azawy.github.io/master/bg/wall4.png"
     },
+
     "chats-mixer": {
         "class": "B",
         "tags": "javascript",
         "text": "Mixes chats from different services",
         "img": "https://github.com/Naheel-Azawy/chats-mixer/raw/master/screenshots/ab.png"
     },
+
     "simplex-calc": {
         "class": "B",
         "tags": "javascript",
@@ -61,101 +70,131 @@ const things = {
         "img": "https://raw.githubusercontent.com/Naheel-Azawy/simplex-calc/master/screenshot.png",
         "link": "https://naheel-azawy.github.io/simplex-calc/"
     },
+
     "moon-script": {
         "class": "B",
         "tags": "sh",
         "text": "Tiny scripts to generate lunar phases GIF",
-        "img": ""
+        "img": "https://raw.githubusercontent.com/Naheel-Azawy/moon-script/master/moon-preview.jpg"
     },
+
     "darken-css": {
-        "class": "C",
-        "tags": "python",
+        "class": "D",
+        "tags": "python,css",
         "text": "Darken CSS already dark style sheets",
         "img": "https://github.com/Naheel-Azawy/darken-css/raw/master/screenshot.png"
     },
+
     "rdf-generate": {
         "class": "D",
         "tags": "javascript",
         "text": "Converts structured and semi-structured data to RDF",
         "img": "https://github.com/Naheel-Azawy/rdf-generate/raw/master/screenshot.png"
     },
+
     "emacs-arabic-reshaper": {
         "class": "C",
         "tags": "emacs-lisp",
         "text": "Reshape Arabic characters in Emacs",
         "img": "https://camo.githubusercontent.com/ae2dcb654679d520404720a53d9881ba9a2084ff/687474703a2f2f6d70636162642e78797a2f77702d636f6e74656e742f75706c6f6164732f323031322f30352f6172616269632d362e706e67"
     },
+
     "c-arabic-reshaper": {
-        "class": "C",
+        "class": "D",
         "tags": "c",
         "text": "A simple Arabic reshaper written in C",
-        "img": ""
+        "img": "https://camo.githubusercontent.com/ae2dcb654679d520404720a53d9881ba9a2084ff/687474703a2f2f6d70636162642e78797a2f77702d636f6e74656e742f75706c6f6164732f323031322f30352f6172616269632d362e706e67"
     },
+
     "BinaryGame": {
         "class": "C",
         "tags": "vala",
         "text": "A tiny binary game written in Vala",
         "img": "https://github.com/Naheel-Azawy/BinaryGame/raw/master/screenshots/s2.png"
     },
+
     "naheel-dotfiles": {
         "class": "B",
         "tags": "desktop,sh",
         "text": "A collection of toys glued together to buildup my setup on my main machine",
         "img": "https://github.com/Naheel-Azawy/naheel-dotfiles/raw/master/screenshots/1.png"
     },
+
     "theterm": {
         "class": "B",
         "tags": "desktop,sh",
         "text": "A wrapper script that gives terminals super powers",
         "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Xterm.png/330px-Xterm.png"
     },
+
     "stpv": {
         "class": "C",
         "tags": "desktop,sh",
         "text": "Simple Terminal PreViewer - Previews as many possible kinds of files",
         "img": "https://raw.githubusercontent.com/Naheel-Azawy/stpv/master/screenshot.png"
     },
+
     "cp-p": {
         "class": "C",
         "tags": "desktop,sh",
-        "text": "",
-        "img": ""
+        "text": "Unix cp (and mv), with progress",
+        "img": "https://raw.githubusercontent.com/Naheel-Azawy/cp-p/master/img.png"
     },
+
     "conky-config": {
         "class": "C",
         "tags": "desktop,sh",
-        "text": "",
-        "img": ""
+        "text": "A binary clock and a long panel - Personal Conky config",
+        "img": "https://raw.githubusercontent.com/Naheel-Azawy/conky-config/master/screenshot.png"
     },
+
     "c-tricks": {
-        "class": "C",
-        "tags": "c",
-        "text": "",
-        "img": ""
+        "class": "D",
+        "tags": "c,fun",
+        "text": "The worst way to use C!",
+        "img": "https://raw.githubusercontent.com/Naheel-Azawy/c-tricks/master/img.png"
     },
+
     "c-oop": {
-        "class": "C",
-        "tags": "c",
-        "text": "",
-        "img": ""
+        "class": "D",
+        "tags": "c,fun",
+        "text": "A little experiment of how object oriented programming can be implemented in C",
+        "img": "https://raw.githubusercontent.com/Naheel-Azawy/c-oop/master/img.png"
     },
+
     "the-super-tiny-compiler-in-c": {
-        "class": "C",
-        "tags": "c",
-        "text": "",
-        "img": ""
+        "class": "D",
+        "tags": "c,fun",
+        "text": "A tiny compiler (< 400 lines) written in C for the purpose of learning",
+        "img": "https://raw.githubusercontent.com/Naheel-Azawy/the-super-tiny-compiler-in-c/master/img.png"
     },
+
     "qu-tad-template": {
         "class": "C",
         "tags": "tex",
         "text": "LaTeX template for Qatar University thesis or dissertation",
         "img": "https://upload.wikimedia.org/wikipedia/en/2/21/Qatar_University_%28logo%29.png"
     },
+
     "ns2-manet-cmp": {
         "class": "C",
         "tags": "networking,ns2,sh",
         "text": "Performance Evaluation of Dynamic Routing Protocols AODV, DSDV, and DSR Using NS-2",
         "img": "https://upload.wikimedia.org/wikipedia/commons/2/2f/Small_Network.png"
-    }
-};
+    },
 
+    "gnome-shell-extension-desktop-editor": {
+        "class": "C",
+        "tags": "gnome,javascript",
+        "text": "Edit .desktop files from GNOME Shell",
+        "img": "https://raw.githubusercontent.com/Naheel-Azawy/gnome-shell-extension-desktop-editor/master/screenshot.png"
+    },
+
+    "adwaita-too-dark": {
+        "class": "C",
+        "tags": "gnome,gtk,css",
+        "text": "Extra dark Adwaita GTK theme",
+        "img": "https://raw.githubusercontent.com/Naheel-Azawy/adwaita-too-dark/master/screenshot.png"
+    }
+
+};
